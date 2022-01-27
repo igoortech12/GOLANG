cebafec098a3410204af4e3d78fb8438692a84e8
igoortech12 <igoortech@gmail.com>
2022-01-27 09:51:17 -0300

    GO PLAYGOUND

---
+++ Aprenda Go - Ellen Korbes/go_playground.go
@@ -0,0 +1,17 @@
+/* BUTTONS 
+RUM = START CODE
+FORMAT = ORGANIZER CODE
+IMPORT = FIND PACKETS IMPORT FOR CODE
+SHARE = SHARE CODE
+*/
+
+// You can edit this code!
+// Click here and start typing.
+package main
+
+import "fmt"
+
+func main() {
+	fmt.Println("Hello, 世界")
+}
+
---
+++ Aprenda Go - Ellen Korbes/readme.txt
@@ -0,0 +1,3 @@
+link course: https://www.youtube.com/watch?v=WiGU_ZB-u0w&list=PLCKpcjBB_VlBsxJ9IseNxFllf-UFEXOdg
+
+Go Playground: https://go.dev/play/
